5c7bd976426046878ddb44d15329289add46d9ac
Michael Mirtenbaum <michael@smithers.local>
2017-03-03 15:34:52 -0500

    DocumentCompilerPass added to conteiner (fix)

--- src/JsonApiBundle.php
+++ src/JsonApiBundle.php
@@ -3,7 +3,9 @@ declare(strict_types = 1);
 
 namespace Mikemirten\Bundle\JsonApiBundle;
 
+use Mikemirten\Bundle\JsonApiBundle\DependencyInjection\DocumentHydratorCompilerPass;
 use Mikemirten\Bundle\JsonApiBundle\DependencyInjection\JsonApiExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JsonApiBundle extends Bundle
@@ -14,5 +16,13 @@ class JsonApiBundle extends Bundle
     public function getContainerExtension()
     {
         return new JsonApiExtension();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DocumentHydratorCompilerPass());
     }
 }

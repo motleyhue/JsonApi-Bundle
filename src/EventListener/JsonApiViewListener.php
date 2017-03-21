fb2b49398281d934a2582eb4777368a5533ee1a8
Michael Mirtenbaum <michael@smithers.local>
2017-03-21 19:09:34 -0400

    JsonApi Document view listener added

---
+++ src/EventListener/JsonApiViewListener.php
@@ -0,0 +1,106 @@
+<?php
+declare(strict_types = 1);
+
+namespace Mikemirten\Bundle\JsonApiBundle\EventListener;
+
+use Mikemirten\Component\JsonApi\Document\AbstractDocument;
+use Mikemirten\Component\JsonApi\Document\ErrorObject;
+use Mikemirten\Component\JsonApi\Document\NoDataDocument;
+use Mikemirten\Component\JsonApi\Document\ResourceObject;
+use Mikemirten\Component\JsonApi\Document\SingleResourceDocument;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+
+/**
+ * JsonApi view listener
+ * Handles a JsonApi-document or its part responded by controller
+ *
+ * @package Mikemirten\Bundle\JsonApiBundle\EventListener
+ */
+class JsonApiViewListener
+{
+    /**
+     * On Kernel View event handler
+     *
+     * @param GetResponseForControllerResultEvent $event
+     */
+    public function onKernelView(GetResponseForControllerResultEvent $event)
+    {
+        $result = $event->getControllerResult();
+
+        if ($result instanceof AbstractDocument) {
+            $event->setResponse($this->createResponse($result));
+            return;
+        }
+
+        if ($result instanceof ResourceObject) {
+            $event->setResponse($this->handleResource($result));
+            return;
+        }
+
+        if ($result instanceof ErrorObject) {
+            $event->setResponse($this->handleError($result));
+        }
+    }
+
+    /**
+     * Handle single resource object
+     *
+     * @param  ResourceObject $resource
+     * @return Response
+     */
+    protected function handleResource(ResourceObject $resource): Response
+    {
+        $document = new SingleResourceDocument($resource);
+
+        return $this->createResponse($document);
+    }
+
+    /**
+     * Handle error
+     *
+     * @param  ErrorObject $error
+     * @return Response
+     */
+    protected function handleError(ErrorObject $error): Response
+    {
+        $document = new NoDataDocument();
+        $document->addError($error);
+
+        return $this->createResponse($document);
+    }
+
+    /**
+     * Create response
+     *
+     * @param  AbstractDocument $document
+     * @return Response
+     */
+    protected function createResponse(AbstractDocument $document): Response
+    {
+        $encoded  = $this->encode($document->toArray());
+        $response = new Response($encoded);
+
+        $response->headers->set('Content-Type', 'application/vnd.api+json');
+
+        return $response;
+    }
+
+    /**
+     * Encode object into a json-string
+     *
+     * @param  mixed $object
+     * @return string
+     * @throws \LogicException
+     */
+    protected function encode($object): string
+    {
+        $encoded = json_encode($object);
+
+        if (json_last_error() === JSON_ERROR_NONE) {
+            return $encoded;
+        }
+
+        throw new \LogicException('Encoding error: ' . json_last_error_msg());
+    }
+}
---
+++ tests/EventListener/JsonApiViewListenerTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Mikemirten\Bundle\JsonApiBundle\EventListener;
+
+use Mikemirten\Component\JsonApi\Document\AbstractDocument;
+use Mikemirten\Component\JsonApi\Document\ErrorObject;
+use Mikemirten\Component\JsonApi\Document\ResourceObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+
+class JsonApiViewListenerTest extends TestCase
+{
+    public function testSkipHandling()
+    {
+        $event = $this->createMock(GetResponseForControllerResultEvent::class);
+
+        $event->expects($this->once())
+            ->method('getControllerResult')
+            ->willReturn([]);
+
+        $event->expects($this->never())
+            ->method('setResponse');
+
+        $listener = new JsonApiViewListener();
+
+        $this->assertNull($listener->onKernelView($event));
+    }
+
+    public function testDocument()
+    {
+        $document = $this->createMock(AbstractDocument::class);
+
+        $document->method('toArray')
+            ->willReturn(['data' => 'qwerty']);
+
+        $event = $this->createEvent($document, '{"data":"qwerty"}');
+
+        $listener = new JsonApiViewListener();
+        $listener->onKernelView($event);
+    }
+
+    public function testResource()
+    {
+        $resource = $this->createMock(ResourceObject::class);
+
+        $resource->method('toArray')
+            ->willReturn(['resource_data' => 'qwerty']);
+
+        $event = $this->createEvent($resource, '{"data":{"resource_data":"qwerty"}}');
+
+        $listener = new JsonApiViewListener();
+        $listener->onKernelView($event);
+    }
+
+    public function testError()
+    {
+        $error = $this->createMock(ErrorObject::class);
+
+        $error->method('toArray')
+            ->willReturn(['error_data' => 'qwerty']);
+
+        $event = $this->createEvent($error, '{"errors":[{"error_data":"qwerty"}]}');
+
+        $listener = new JsonApiViewListener();
+        $listener->onKernelView($event);
+    }
+
+    /**
+     * Create mock of event
+     *
+     * @param  $result
+     * @param  string $expectedContent
+     * @return GetResponseForControllerResultEvent
+     */
+    protected function createEvent($result, string $expectedContent): GetResponseForControllerResultEvent
+    {
+        $event = $this->createMock(GetResponseForControllerResultEvent::class);
+
+        $event->expects($this->once())
+            ->method('getControllerResult')
+            ->willReturn($result);
+
+        $event->expects($this->once())
+            ->method('setResponse')
+            ->with($this->isInstanceOf(Response::class))
+            ->willReturnCallback(
+                function(Response $response) use($expectedContent)
+                {
+                    $this->assertSame(
+                        $expectedContent,
+                        $response->getContent()
+                    );
+                }
+            );
+
+        return $event;
+    }
+}
